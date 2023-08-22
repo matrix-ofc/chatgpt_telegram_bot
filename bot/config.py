557aa30c40364c2bc8e5393d5d6abf8e4505f0a2
matrix-ofc <135301029+matrix-ofc@users.noreply.github.com>
2023-08-22 08:09:49 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -12,8 +12,8 @@ with open(config_dir / "config.yml", 'r') as f:
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
+telegram_token = config_yaml["6159960939:AAEGQZtSt1xdMPKXtWxTzPzqOxzKpKp_cRM"]
+openai_api_key = config_yaml["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"]
 openai_api_base = config_yaml.get("openai_api_base", None)
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
